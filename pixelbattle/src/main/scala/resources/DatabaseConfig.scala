e996a529c3703cf8a805e87bcfca16d62bb32c16
Ксения Иванова <natimal.well@gmail.com>
2025-03-25 21:33:46 +0400

    Доделки

---
+++ pixelbattle/src/main/scala/resources/DatabaseConfig.scala
@@ -0,0 +1,10 @@
+package resources
+
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
+import cats.effect.{IO, Resource}
+import scala.concurrent.ExecutionContext
+
+object DatabaseConfig{
+  
+}
